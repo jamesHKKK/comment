ecd1a0257391b5a468c24f51d1047942bb4996b9
QianJiaji <1930754466@qq.com>
2020-08-20 14:14:55 +0800

    add

---
+++ comment.js
@@ -0,0 +1,15 @@
+//获取当前年月日，并格式化
+function getYearMonthDay() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+    var nowDate = year + "-" + month + "-" + day + " ";
+    return nowDate;
+}
